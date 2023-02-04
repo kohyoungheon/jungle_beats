7b08749ab69b8f52aedd2d93476d4079a6b188f7
Young Koh <youngheon6@gmail.com>
2023-02-04 15:00:51 -0700

    add all method

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -3,20 +3,25 @@ class JungleBeat
   def initialize
     @list = LinkedList.new
   end
+
   def append(beats)
-    beats_array = beats.split
+    beats_added=0
+    beats_array = beats.split(" ")
     beats_array.each do |beat|
-      @list.append(beat)
+      if ["tee","dee","deep","bop","boop","la","na"].include?(beat)
+        @list.append(beat)
+        beats_added += 1
+      end
     end
-    @list
+    puts beats_added.to_s << " beats added"
   end
+
   def count
     count = 0
     current_node = @list.head
     while current_node
       count +=1
       current_node = current_node.next_node
-      # require 'pry'; binding.pry
     end
     count
   end
@@ -24,5 +29,10 @@ class JungleBeat
   def play
     beats = @list.to_string
     `say -r 500 -v Boing #{beats}`
+  end
+
+  def all
+    beats = @list.to_string
+    puts beats
   end
 end
--- lib/runner.rb
+++ lib/runner.rb
@@ -3,9 +3,10 @@ require './linked_list'
 require "./jungle_beat"
 
 jb = JungleBeat.new
-jb.append("deep doo ditt woo hoo shu")
-jb.play
-require 'pry'; binding.pry
+jb.append("tee tee")
+jb.append("Mississippi")
+jb.all
+# require 'pry'; binding.pry
 # require 'pry'; binding.pry
 # `say -r 500 -v Boing #{jb.list.to_string}`
 # require 'pry'; binding.pry
